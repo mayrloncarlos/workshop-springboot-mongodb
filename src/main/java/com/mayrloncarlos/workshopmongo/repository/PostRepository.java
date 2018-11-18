e898daba7aaed3090de1d490257f3f2ad338ec34
mayrloncarlos <mayrloncarlos@gmail.com>
2018-11-18 12:55:39 -0300

    Consulta simples com @query

--- src/main/java/com/mayrloncarlos/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/mayrloncarlos/workshopmongo/repository/PostRepository.java
@@ -13,10 +13,9 @@ import com.mayrloncarlos.workshopmongo.domain.User;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
 
-/*	
+	
 	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
 	List<Post> searchTitle(String text);
-*/	
 	
 	List<Post> findByTitleContainingIgnoreCase(String text);
 	
